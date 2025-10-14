d16a9028ed7e6be203f7379987f01f762b6bb0b2
Hamza Khan <lalahamza327@gmail.com>
2025-10-14 10:57:24 +0500

    navbar adjustments

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import '../css/app.css';
 import './bootstrap';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../../public/vendors/bootstrap/js/bootstrap.bundle.min.js';
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
